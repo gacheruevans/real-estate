99d2aa975c856dc6800e874bd318d58bdf80e91e
Evans Gacheru <evans.gacheru@andela.com>
2023-11-03 23:49:23 +0300

    image upload functioonality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -22,8 +22,7 @@ export default function CreateListing() {
         pets: false,
     });
     const [imageUploadError, setImageUploadError] = useState(false);
-    console.log(formData);
-    const dispatch = useDispatch();
+    const [ uploading, setUploading ] = useState(false);
     const handleChange = async (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
@@ -54,18 +53,28 @@ export default function CreateListing() {
     const handleImageSubmit = (e) => {
         e.preventDefault();
         if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+            setUploading(true);
+            setImageUploadError(false);
+            
             const promises = [];
+            
             for (let i = 0; i < files.length; i++) {
                 promises.push(storeImage(files[i]));
             }
-            Promise.all(promises).then((urls) => {
-                setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) });
-                setImageUploadError(false);
-            }).catch((err) => {
-                setImageUploadError("Image Upload Failed (2 MB Max per Image)");
-            });
+            Promise.all(promises)
+                .then((urls) => {
+                    setFormData({ 
+                        ...formData, 
+                        imageUrls: formData.imageUrls.concat(urls) 
+                    });
+                    setImageUploadError(false);
+                    setUploading(false);
+                }).catch((err) => {
+                    setImageUploadError("Image Upload Failed (2 MB Max per Image)");
+                });
         }else {
             setImageUploadError("You can only upload 6 images per listing.");
+            setUploading(false);
         }
     };
 
@@ -86,6 +95,12 @@ export default function CreateListing() {
                     });
                 }
             )
+        });
+    };
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i !== index),
         });
     };
   return (
@@ -212,10 +227,26 @@ export default function CreateListing() {
                     </p>
                     <div className="flex gap-4">
                         <input onChange={(e) => setFiles(e.target.files) } className="w-full p-3 border border-gray-300 rounded" type="file" id="images" accept="image/*" multiple />
-                        <button type="button" onClick={handleImageSubmit} className="p-3 text-green-700 uppercase border rounded boorder-green-700 hover:shadow-lg disabled:opacity-80">Upload</button>
+                        <button 
+                            type="button"
+                            disabled={uploading} 
+                            onClick={handleImageSubmit} 
+                            className="p-3 text-green-700 uppercase border rounded boorder-green-700 hover:shadow-lg disabled:opacity-80">
+                            { uploading ? "Uploading..." : "Uplooad" }
+                        </button>
                     </div>
                     <p className="text-sm text-red-700">{imageUploadError && imageUploadError}</p>
+                    {
+                        formData.imageUrls.length > 0 && FormData.imageUrls.map((url, index) => (
+                            <div key={url} className="flex items-center justify-between p-3 border">
+                                <img src={url} alt="listing image" className="object-contain w-20 h-20 rounded-lg" />
+                                <button className="p-3 text-red-700 uppercase rounded-lg hover:opacity-75">Delete</button>
+                            </div>
+                        ))
+                    }
                     <button 
+                        type="button"
+                        onClick={ () =>handleRemoveImage(index)}
                         className="py-2.5 px-4 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">Create Listing
                     </button>
                 </div>
