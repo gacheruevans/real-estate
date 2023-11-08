2093bd8dc0f7300ced4fac2ede8dc9c7de5ef855
Evans Gacheru <evans.gacheru@andela.com>
2023-11-08 19:34:09 +0300

    refactor code

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { createListingStart, createListingSuccess, createListingFailure } from "../redux/listing/listingSlice.js";
+import { useSelector } from "react-redux";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from "../firebase.js";
 
 export default function CreateListing() {
+    const { currentUser } = useSelector(state => state.user);
     const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
         imageUrls: [],
@@ -21,37 +22,47 @@ export default function CreateListing() {
         furnished: false,
         pets: false,
     });
+    console.log(formData);
     const [imageUploadError, setImageUploadError] = useState(false);
-    const [ uploading, setUploading ] = useState(false);
+    const [uploading, setUploading] = useState(false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleChange = async (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
+        setFormData({ 
+            ...formData, 
+            [e.target.id]: e.target.value,
+        });
     };
 
-    const handlesubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            dispatch(createListingStart());
+           
+            setLoading(true);
+            setError(false);
             const res = await fetch('api/listing/create',{
                 method: 'POST',
                 headers:{
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(formData),
+                body: JSON.stringify({
+                    ...formData, 
+                    userRef: currentUser._id,
+                }),
             });
-            const data = await res.json(data);
-
+            const data = await res.json();
             if (data.success === false) {
-                dispatch(createListingFailure(data.message));
+                setError(error.message);
                 return;
             }
-            dispatch(createListingSuccess(data));
         } catch (error) {
-            dispatch(createListingFailure(error.message));
+            setError(error.message);
+            setLoading(false);
         }
     };
 
-    const handleImageSubmit = (e) => {
-        e.preventDefault();
+    const handleImageSubmit = () => {
+       
         if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
             setUploading(true);
             setImageUploadError(false);
@@ -61,17 +72,18 @@ export default function CreateListing() {
             for (let i = 0; i < files.length; i++) {
                 promises.push(storeImage(files[i]));
             }
-            Promise.all(promises)
-                .then((urls) => {
-                    setFormData({ 
-                        ...formData, 
-                        imageUrls: formData.imageUrls.concat(urls) 
-                    });
-                    setImageUploadError(false);
-                    setUploading(false);
-                }).catch((err) => {
-                    setImageUploadError("Image Upload Failed (2 MB Max per Image)");
+
+            Promise.all(promises).then((urls) => {
+                setFormData({ 
+                    ...formData, 
+                    imageUrls: formData.imageUrls.concat(urls), 
                 });
+                setImageUploadError(false);
+                setUploading(false);
+            // eslint-disable-next-line no-unused-vars
+            }).catch((error) => {
+                setImageUploadError("Image Upload Failed (2 MB Max per Image)");
+            });
         }else {
             setImageUploadError("You can only upload 6 images per listing.");
             setUploading(false);
@@ -86,6 +98,10 @@ export default function CreateListing() {
             const uploadTask = uploadBytesResumable(storageRef, file);
             uploadTask.on(
                 "state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
                 (error) => {
                     reject(error);
                 },
@@ -97,6 +113,7 @@ export default function CreateListing() {
             )
         });
     };
+
     const handleRemoveImage = (index) => {
         setFormData({
             ...formData,
@@ -106,7 +123,7 @@ export default function CreateListing() {
   return (
     <main className="max-w-4xl p-3 mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
-            <form onSubmit={handlesubmit} className="flex flex-col gap-4 sm:flex-row">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 sm:flex-row">
                 <div className="flex flex-col flex-1 gap-4">
                     <input 
                         type="text" 
@@ -117,7 +134,7 @@ export default function CreateListing() {
                         minLength="10"
                         required
                         placeholder="Name" />
-                    <input 
+                    <textarea 
                         type="text" 
                         id="description" 
                         className="block w-full h-10 px-3 mt-2 bg-white rounded-md shadow-sm appearance-none text-slate-900 sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200"  
@@ -192,19 +209,37 @@ export default function CreateListing() {
                         <div className="flex items-center gap-2">
                             <input 
                                 className="p-3 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none" 
-                                type="number" id="bedrooms" min="1" max="10" required/>
+                                type="number" 
+                                id="bedrooms" 
+                                min="1" 
+                                max="10" 
+                                required
+                                onChange={handleChange}
+                                value={formData.bedrooms} />
                             <p>Beds</p>
                         </div>
                         <div className="flex items-center gap-2">
                             <input 
                                 className="p-3 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none" 
-                                type="number" id="bathrooms" min="1" max="10" required/>
+                                type="number" 
+                                id="bathrooms" 
+                                min="1" 
+                                max="10" 
+                                required
+                                onChange={handleChange} 
+                                value={formData.bathrooms} />
                             <p>Baths</p>
                         </div>
                         <div className="flex items-center gap-2">
                             <input 
                                 className="p-3 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none" 
-                                type="number" id="regular_price" min="1" max="10" required/>
+                                type="number" 
+                                id="regular_price" 
+                                min="1" 
+                                max="10" 
+                                required
+                                onChange={handleChange} 
+                                value={formData.regularPrice} />
                             <div className="flex flex-col items-center">
                                 <p>Regular Price</p>
                                 <span className="text-xs">($ / month)</span>
@@ -213,7 +248,13 @@ export default function CreateListing() {
                         <div className="flex items-center gap-2">
                             <input 
                                 className="p-3 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none" 
-                                type="number" id="discount_price" min="1" max="10" required/>
+                                type="number" 
+                                id="discount_price" 
+                                min="50" 
+                                max="10000000" 
+                                required
+                                onChange={handleChange}
+                                value={formData.discountPrice}/>
                             <div className="flex flex-col items-center">
                                 <p>Discounted Price</p>
                                 <span className="text-xs">($ / month)</span>
@@ -226,18 +267,26 @@ export default function CreateListing() {
                         <span className="ml-2 font-normal text-gray-600">The first image will be the cover (max 6)</span>
                     </p>
                     <div className="flex gap-4">
-                        <input onChange={(e) => setFiles(e.target.files) } className="w-full p-3 border border-gray-300 rounded" type="file" id="images" accept="image/*" multiple />
+                        <input 
+                            onChange={(e) => setFiles(e.target.files) } 
+                            className="w-full p-3 border border-gray-300 rounded" 
+                            type="file" 
+                            id="images" 
+                            accept="image/.*" 
+                        multiple />
                         <button 
                             type="button"
                             disabled={uploading} 
                             onClick={handleImageSubmit} 
                             className="p-3 text-green-700 uppercase border rounded boorder-green-700 hover:shadow-lg disabled:opacity-80">
-                            { uploading ? "Uploading..." : "Uplooad" }
+                            { uploading ? "Uploading..." : "Upload" }
                         </button>
                     </div>
                     <p className="text-sm text-red-700">{imageUploadError && imageUploadError}</p>
                     {
-                        formData.imageUrls.length > 0 && FormData.imageUrls.map((url, index) => (
+                        // eslint-disable-next-line no-unused-vars
+                        
+                        formData.imageUrls.length > 0 && formData.imageUrls.map((url) => (
                             <div key={url} className="flex items-center justify-between p-3 border">
                                 <img src={url} alt="listing image" className="object-contain w-20 h-20 rounded-lg" />
                                 <button className="p-3 text-red-700 uppercase rounded-lg hover:opacity-75">Delete</button>
@@ -245,12 +294,14 @@ export default function CreateListing() {
                         ))
                     }
                     <button 
+                        disabled={loading}
                         type="button"
-                        onClick={ () =>handleRemoveImage(index)}
-                        className="py-2.5 px-4 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">Create Listing
+                        onClick={ () => handleRemoveImage(index)}
+                        className="py-2.5 px-4 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
+                            { loading ? 'Creating...' : 'Create Listing' }
                     </button>
+                    {error && <p className="text-red-700 text-sm">{ error }</p>}
                 </div>
-                
             </form>
     </main>
   )
