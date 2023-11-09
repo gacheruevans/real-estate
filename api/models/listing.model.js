d6e343ac09c525879f37af0c5d4bb1cdb17dc53e
Evans Gacheru <evans.gacheru@andela.com>
2023-11-09 19:31:16 +0300

    listings layout

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -51,7 +51,7 @@ const listingSchema = new mongoose.Schema({
     },
     imageUrls: {
         type: Array,
-        default: "https://pixabay.com/get/g62368fcafe7f2b4e6bc42880e16215c6b5c338d67e9605303735ab9c4391d72ff8b3d300dc461e103da3edf00c0ff461d0733ab7dce911e47439841d8dcd1998b932d7c7fa6f86d20463303cfe97ae93_640.png",
+        default: "../../public/images/home.png",
         required: true,
     },
     userRef: {
