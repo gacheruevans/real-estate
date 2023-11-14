e1371cd33fd6ace052c4501bb46ae64246dd9623
Evans Gacheru <evans.gacheru@andela.com>
2023-11-14 20:01:31 +0300

    add swipper package and slider component

--- client/src/components/ListingDetails.jsx
+++ client/src/components/ListingDetails.jsx
@@ -1,5 +1,10 @@
-import { useRef, useEffect, useState } from "react";
-import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
 import Sidebar, { SidebarItem } from "../components/Sidebar";
 import {
     Archive,
@@ -13,59 +18,61 @@ import {
     Settings,
     LayoutDashboard,
 } from "lucide-react";
-import mapboxgl from "mapbox-gl";
+// import mapboxgl from "mapbox-gl";
 
 export default function ListingDetails() {
+    SwiperCore.use([Navigation]);
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     const params = useParams();
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(36.611686);
-    const [lat, setLat] = useState(-1.264090);
-    const [zoom, setZoom] = useState(11);
-    
-    const [listingDetailsError, setlistingDetailsError] = useState(false);
-    const [listingDetails, setListingDetails] = useState([]);
+    // const mapContainer = useRef(null);
+    // const map = useRef(null);
+    // const [lng, setLng] = useState(36.611686);
+    // const [lat, setLat] = useState(-1.264090);
+    // const [zoom, setZoom] = useState(11);
 
     useEffect(() => {
         const fetchListing = async () => {
             try{
-                setlistingDetailsError(false);
-                
+                setLoading(true);
                 const listingId = params.listingId;
                 const res = await fetch(`/api/listing/getListing/${listingId}`);
                 const data = await res.json();
 
                 if(data.success === false) {
-                    setlistingDetailsError(true);
+                    setError(true);
+                    setLoading(false);
                     return;
                 }
-                
-                setlistingDetailsError(false);
-                setListingDetails(data);
+                setListing(data);
+                setLoading(false);
+                setError(false);
             } catch (error) {
-                setlistingDetailsError(true);
+                setError(true);
+                setLoading(false);
             }
         };
 
         fetchListing();
 
-        if (map.current) return; // initialize map only once
-            map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-      }, [lat, lng, zoom, params.listingId]);
+        // if (map.current) return; // initialize map only once
+        //     map.current = new mapboxgl.Map({
+        //     container: mapContainer.current,
+        //     style: 'mapbox://styles/mapbox/streets-v12',
+        //     center: [lng, lat],
+        //     zoom: zoom
+        // });
+        // map.current.on('move', () => {
+        //     setLng(map.current.getCenter().lng.toFixed(4));
+        //     setLat(map.current.getCenter().lat.toFixed(4));
+        //     setZoom(map.current.getZoom().toFixed(2));
+        // });
+      }, [params.listingId]);
 
-      const addDefaultSrc = (ev) => {
-        ev.target.src = "../../public/images/home.png";
-      };
+    //   const addDefaultSrc = (ev) => {
+    //     ev.target.src = "../../public/images/home.png";
+    //   };
   return (
     <>
         <div className="flex w-full columns-2">
@@ -87,10 +94,30 @@ export default function ListingDetails() {
                 <Link to="/dashboard/settings"><SidebarItem icon={<Settings size={20} />} text="Settings"  /></Link>
                 <Link to="/dashboard/help"><SidebarItem icon={<LifeBuoy size={20} />} text="Help"  /></Link>
             </Sidebar>
-            <div className="">
-                <div ref={mapContainer} className="map-container" />
-            </div>
-            <div className="flex w-full min-h-screen bg-slate-800">
+           
+            <div className="flex w-full min-h-screen">
+                {
+                    loading && <p className="text-2xl text-center my-7"> Loading...</p>
+                }
+                {
+                    error && <p className="text-2xl text-center my-7"> Oooops... Something Went Wrong!</p>
+                }
+                {
+                    listing && !loading && !error && (
+                       
+                            <Swiper navigation>
+                                {listing.imageUrls.map( (url) => (
+                                    <SwiperSlide key={url}>
+                                            <img
+                                                className="h-[550px] w-full object-cover" 
+                                                src={url}
+                                            />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                       
+                    )
+                }
             </div>
         </div>
     </>
