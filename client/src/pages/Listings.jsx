60a33359f72b5d3626d24c930f3e68dd28a09218
Evans Gacheru <evans.gacheru@andela.com>
2024-01-05 14:25:06 +0300

    add geojson file

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -14,37 +14,70 @@ import {
     Settings,
     LayoutDashboard,
 } from "lucide-react";
-import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = "pk.eyJ1IjoiZWdhY2hlcnUiLCJhIjoiY2xvczlzZnJ6MHozcjJqbXpvNXRreW56aCJ9.fL6OWHyFGY01vWKdowHKrQ";
+
+import mapboxgl from "mapbox-gl";
+import geoJson from "../data/geo.json";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZWdhY2hlcnUiLCJhIjoiY2xxemYybzZ0MDExZDJpbGpoMDgyZHZvMSJ9.5q11xHIeWzA94kV2p-iiBg";
+
 export default function Listings() {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(36.611686);
     const [lat, setLat] = useState(-1.264090);
-    const [zoom, setZoom] = useState(11);
-
+    const [zoom, setZoom] = useState(15);
+    
     const {currentUser} = useSelector((state) => state.user);
     const [showListingsError, setShowListingsError] = useState(false);
     const [userListings, setUserListings] = useState([]);
-
     
     useEffect(() => {
         if (map.current) return; // initialize map only once
             map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [lng, lat],
+                zoom: zoom
+            });  
+        
+        
+        // Create default markers
+        geoJson.features.map((feature) =>
+            new mapboxgl.Marker()
+                .setLngLat(feature.geometry.coordinates)
+                .setPopup(
+                    new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML(
+                    `<img src=${feature.properties.image} /><h2>${feature.properties.title}</h2><p>${feature.properties.description}</p>`
+                    )
+                )
+                .addTo(map.current)
+        );
+
+        // Add a new layer to visualize the polygon.
+        map.current.addLayer({
+            'id': 'maine',
+            'type': 'fill',
+            'source': 'maine', // reference the data source
+            'layout': {},
+            'paint': {
+            'fill-color': '#0080ff', // blue color fill
+            'fill-opacity': 0.5
+            }
+        });  
+
+        // navigation controls +/- zoom buttons
+        const nav = new mapboxgl.NavigationControl();
+        map.current.addControl(nav, 'top-right');
+        
         map.current.on('move', () => {
             setLng(map.current.getCenter().lng.toFixed(4));
             setLat(map.current.getCenter().lat.toFixed(4));
             setZoom(map.current.getZoom().toFixed(2));
         });
-
+        
         const handleShowListings = async () => {
-
             try {
                 setShowListingsError(false);
                 const res = await fetch(`/api/user/listing/${currentUser._id}`);
@@ -60,11 +93,12 @@ export default function Listings() {
             }
         };
         handleShowListings();
-      }, [lat, lng, currentUser._id, zoom]);
+    }, [currentUser._id, lng, lat, zoom]);
 
-      const addDefaultSrc = (ev) => {
-        ev.target.src = "../../public/images/home.png";
-      };
+    const addDefaultSrc = (ev) => {
+        ev.target.src = "../../images/home.png";
+    };
+    
   return (
     <>
         <div className="flex w-full columns-2">
@@ -86,9 +120,10 @@ export default function Listings() {
                 <Link to="/dashboard/settings"><SidebarItem icon={<Settings size={20} />} text="Settings"  /></Link>
                 <Link to="/dashboard/help"><SidebarItem icon={<LifeBuoy size={20} />} text="Help"  /></Link>
             </Sidebar>
-            <div className="">
+            <div>
                 <div ref={mapContainer} className="map-container" />
             </div>
+        
             <div className="flex w-full min-h-screen bg-slate-800">
                 <div className="absolute right-0 mt-2">
                     <Link 
